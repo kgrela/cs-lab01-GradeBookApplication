557584201dc3aae2f9418f0ddd02dfaa860cde44
kacper.grela <kacper.grela@PK4-07>
2022-03-17 16:31:56 +0100

    Created GetLetterGrade, CalculateStatistics, CalculateStudentStatistics methods; fixed constructors; made BaseGradeBook class abstract; added IsWeighted property;

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -8,9 +8,76 @@ namespace GradeBook.GradeBooks
 {
     public class RankedGradeBook : BaseGradeBook
     {
-        public RankedGradeBook(string name) : base(name)
+        public RankedGradeBook(string name, bool enable) : base(name)
         {
             Type = GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw InvalidOperationException;
+            }
+
+            int sortStudent = 0;
+
+            foreach (var student in Students)
+            {
+                if (student.AverageGrade > averageGrade)
+                {
+                    sortStudent++;
+                }
+            }
+
+            if (sortStudent < Students.Count / 5)
+            {
+                return 'A';
+            }
+            else if (sortStudent < 2 * (Students.Count / 5))
+            {
+                return 'B';
+            }
+            else if (sortStudent < 3 * (Students.Count / 5))
+            {
+                return 'C';
+            }
+            else if (sortStudent < 4 * (Students.Count / 5))
+            {
+                return 'D';
+            }
+            else
+            {
+                return 'F';
+            }
+        }
+
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students!");
+            }
+            else
+            {
+                base.CalculateStatistics();
+            }
+
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students!");
+            }
+            else
+            {
+                base.CalculateStudentStatistics(name);
+            }
+
+        }
+
+        public Exception InvalidOperationException { get; set; }
     }
 }
